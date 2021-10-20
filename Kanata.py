3032a7353d08c22300a6c27feb00abd04b54de46
SingleDog <47451036+xDDoubleTea@users.noreply.github.com>
2021-10-20 23:51:59 +0800

    Add files via upload

--- Kanata.py
+++ Kanata.py
@@ -17,12 +17,14 @@ import json
 import pytube
 #import time
 
+Me = 398444155132575756
 Member = 489568072483471372
 
 Pi = math.pi
 
 client_id = 865465022264377376
 client_secret = 'QWC5PDZyxNgWoE_A0p-DsLgRbqqNh-sf'
+version = 1.0
 
 
 youtube_dl.utils.bug_reports_message = lambda: ''
@@ -80,7 +82,7 @@ def get_prefix(client , message):
 
 pre = 'a!'
 #intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
-client = commands.Bot(command_prefix = pre)
+client = commands.Bot(command_prefix = pre) #, help_command=None
 #client.remove_command('help')
 MusicManager = MusicManager(client, client_id=client_id, client_secret=client_secret)
 
@@ -95,8 +97,8 @@ queueadd = []
 nowplaying = 0
 loop_state = False
 joined = False
-
-
+default_footer = "Bot Developed by Hoshiyomi#6942"
+default_footer_icon = "https://cdn.discordapp.com/avatars/398444155132575756/77db70f07858b08a72896f248e2ffcaf.webp?size=4096"
 
 
 
@@ -142,33 +144,34 @@ async def on_guild_remove(guild):
 @client.event
 async def on_member_join(member):
     joined_guild = member.guild
-    await joined_guild.send(f'Welcome to {member.guild.name}!!')
-    #await member.add_roles(member.guild.get_role(Member))
+    channel = joined_guild.get_channel(489439897267077131)
+    await channel.send(f'Welcome to {joined_guild.name}!!')
 
 
 @client.event
 async def on_message(message):
+    ctx = message.content
     if message.author == client.user:
         return 
-    
 
-    elif message.content.startswith('Hello!!'):
+
+    elif ctx.startswith('Hello!!'):
             await message.channel.send('Hello!!')
         
-    elif message.content.startswith('hi'):
+    elif ctx.startswith('hi'):
         await message.channel.send('Hi!!')
 
-    elif message.content.startswith('.'):
+    elif ctx.startswith('.'):
         await message.channel.send('不要句點我啦QQ')
 
-    elif message.content.startswith('Your chest is small'):
+    elif ctx.startswith('Your chest is small'):
         responses = ['You wanna die huh?' ,'Ill kill you', 'I thoght you love me!' , 'No. Im boing boing', 'Whatever you say.' ,'大嫌い!!']
         await message.channel.send(random.choice(responses))
         
-    elif message.content.startswith('yay') or message.content.startswith('Ya') or message.content.startswith('YA'):
+    elif ctx.startswith('yay') or ctx.startswith('Ya') or ctx.startswith('YA'):
         responses = ["I'm glad that you're happy!" , "yay!!!", 'Yay!!!', "I'm happy too!!" ]
         await message.channel.send(random.choice(responses))
-        
+    
         #elif message.content.startswith(''):
             
 
@@ -185,9 +188,24 @@ async def on_member_remove(member):
 commands
 '''
 
-@client.command(help='Returns help menu')
+@client.command(aliases = ["hlep"], help='Returns help menu')
 async def h(ctx):
-    await ctx.send('Help menu:')
+    
+    helpmenu= discord.Embed(
+        title = "Click me to see list of commands",
+        description = "Help menu",
+        url = "https://hackmd.io/@Kawaii-kanataso/ry_w9QXVK",
+        colour = discord.Color.blue()
+    )
+    helpmenu.set_author(name = f"{client.user}")
+    helpmenu.set_thumbnail(url = 
+    "https://cdn.discordapp.com/attachments/672102728469577785/891721492671389696/20210710_223830.jpg")
+    helpmenu.add_field(
+        name = "If you need any help you can message me on Discord" , 
+        value = "Hoshiyomi#6942", 
+        inline = True)
+    helpmenu.set_footer(text = "Bot Developed by Hoshiyomi#6942" ,icon_url= "https://cdn.discordapp.com/avatars/398444155132575756/77db70f07858b08a72896f248e2ffcaf.webp?size=4096")
+    await ctx.send(embed=helpmenu)
 
 @client.command(help = 'Returns the latency of the bot')
 async def ping(ctx):
@@ -217,7 +235,7 @@ async def _8ball(ctx, *, question):
                 'I thought too hard and died.'
                 ,'I love you!'
                 ,'Hi Im good']
-    await ctx.send(f'Asked:{question}\n{random.choice(responses)}')
+    await ctx.send(f'{random.choice(responses)}')
 
 @client.command(help = 'Returns a random number')
 async def dice(ctx, ground :int, limit :int):
@@ -225,9 +243,27 @@ async def dice(ctx, ground :int, limit :int):
 
 @client.command(aliases = ["crd","crds"], help = "Returns the credits")
 async def credits(ctx):
-    await ctx.send("Bot coded by Imhoshiyomi#6942")
-    await ctx.send("Version 1.0")
-    await ctx.send("Do not abuse it or I might kill you.")
+    embed = discord.Embed(
+        title = "Bot coded by ImHoshiyomi#6942",
+        description = f"Version {version}",
+        color = discord.Colour.blue()
+    )
+    embed.set_footer(text="Do not abuse it or I might kill you.")
+    await ctx.send(embed = embed)
+    
+
+@client.command(aliases = ["info"], help = "Returns the bot information")
+async def information(ctx):
+    
+    info = discord.Embed(
+        title = f"{client.user}",
+        description = f"Version {version}",
+        color = discord.Colour.blue()
+    )
+    info.set_thumbnail(url = 
+    "https://cdn.discordapp.com/attachments/672102728469577785/891721492671389696/20210710_223830.jpg")
+    info.add_field(name = "Author", value = "Hoshiyomi#6942")
+    await ctx.send(embed=info)
 
 
 
@@ -248,9 +284,6 @@ async def displayembed(ctx):
         title = "title",
         description = "This is a DESC",
         colour = discord.Colour.blue()
-
-
-
     )
     embed.set_footer(text = 'This is a footer')
     embed.set_image(url = 'https://media.discordapp.net/attachments/860187581445439500/891252962733719562/92644823_p0.png?width=410&height=701')
@@ -264,6 +297,11 @@ async def displayembed(ctx):
     await ctx.send(embed=embed)
     
 
+@client.command(help = "dont use it")
+async def say(ctx,*,sentence):
+    await ctx.channel.purge(limit = 1)
+    await ctx.send(sentence)
+
 
 
 '''
@@ -273,8 +311,7 @@ Music
 
 @MusicManager.event
 async def on_play(ctx, player):
-    player = await MusicManager.now_playing(ctx)
-    await ctx.send(f"Now playing: {player}")
+    await ctx.send(f"Now playing: {player.title}")
 
 @client.command(aliases = ["pau"],help =  "Pauses the current song playing")
 async def pause(ctx):
@@ -323,12 +360,42 @@ async def _queue(ctx):
     global queue_url
     global queueadd
     global queue
-    if  len(queue)>0:
-        await ctx.send("Now playing queue:\n")
-        i = 1
+    if player := await MusicManager.now_playing(ctx):
+        queueembed = discord.Embed(
+            title = "Now playing queue:" , color = discord.Colour.blue()
+        )
+        queuenow = i = find = 0
+
+        for j in queue:
+            if j.title == player.title:
+                queuenow = find
+            find+=1
+        
+        for x in range(0,queuenow):
+            remove = queue[x]
+            queue.remove(remove)
+            remove = queue_url[x]
+            queue_url.remove(remove)
+            remove = queueadd[x]
+            queueadd.remove(remove)
+
         for x in queue:
-            await ctx.send(f"<{i}> {x.title}, added by <@!{queueadd[i-1]}> \n Video url : {queue_url[i-1]}")
+            if x.title == player.title:
+                queueembed.add_field(
+                        name = f"(Now){x.title}", 
+                        value = f"added by <@!{queueadd[i]}>",
+                        inline = False
+                    )
+            else:
+                queueembed.add_field(
+                        name = f"<{(i+1)-queuenow}> {x.title}", 
+                        value = f"added by <@!{queueadd[i]}>",
+                        inline = False
+                    )
+                    
             i += 1
+        
+        await ctx.send(embed = queueembed)
     else:
         await ctx.send("The queue is empty. Use the play command to add some songs to queue")
 
@@ -337,15 +404,17 @@ async def skip(ctx):
     if await MusicManager.skip(ctx):
         await ctx.send("Skipped!")
     else:
-        return
+        await ctx.send("This is the end of the queue!")
     
 
 @client.command(help = "Joins the voice channel that you're currently in")
 async def join(ctx):
     user = ctx.message.author
+    global joined
     try:
         await user.voice.channel.connect()
         await ctx.send("Joined Voice Channel")
+        joined = True
     except:
         await ctx.send("An error occured. Please try again later")
 
@@ -364,7 +433,7 @@ async def play(ctx, *, url: str):
         await ctx.send("You're not connected to a voice channel")
         joined = False
         return
-    elif joined == False:
+    elif not joined:
         channel = user.voice.channel
         await channel.connect()    
         await ctx.send("Joined Voice Channel")
@@ -379,21 +448,29 @@ async def play(ctx, *, url: str):
     queue_url.insert(len(queue_url) , url)
     queueadd.insert(len(queueadd), user.id)
     await MusicManager.play(ctx)
+
+    '''
+    embed
+    '''
+
     nowplayingembed = discord.Embed(
-                title = "**Added to queue:**",
-                description = f"Requested by {user.mention}",
+                title = f"{player2.title}",
+                description = "**Added to queue** ",
+                url = f'{url}',
                 colour = discord.Colour.blue()
                 )
-    nowplayingembed.set_footer(text = f"{url}")
     nowplayingembed.set_image(url = f'{yt.thumbnail_url}')
-    nowplayingembed.add_field(name = f"Video title:", value = f"**{player2.title}**")
-            #await ctx.send(f'**Now Playing:**  {player2.title}, added by <@!{user}>')
+    nowplayingembed.add_field(name = f"Requested by", value = f'{user.mention}')
+    #nowplayingembed.add_field(name = f"Video:", value = '')
+    #await ctx.send(f'**Now Playing:**  {player2.title}, added by <@!{user}>')
+            
     await ctx.send(embed = nowplayingembed)
 
 
 @client.command(aliases = ['vol'] , help = 'Controls the volume of the bot')
 async def volume(ctx, volume: int):
-    await MusicManager.volume(ctx, volume)
+    if await MusicManager.volume(ctx, volume):
+        await ctx.send(f"Volume has been set to {volume}")
 
 @client.command(aliases = ["l"], help = "Loops the current queue")
 async def loop(ctx):
@@ -407,11 +484,62 @@ async def loop(ctx):
 @client.command(aliases = ["stp"], help = "Stops the current player and disconnects")
 async def stop(ctx):
     global joined
+    global queue
+    global queue_url
+    global queueadd
+    queue.clear()
+    queueadd.clear()
+    queue_url.clear()
     user = ctx.message.author
     ctx.voice_client.pause()
     await ctx.send(f"Player has been stopped by <@!{user.id}>")
     await ctx.voice_client.disconnect()
     joined = False
+
+
+'''
+Admin commands
+'''
+
+@client.command(aliases = ["ld"], help = "<ADMIN only command> Load extension")
+@commands.has_permissions(administrator = True)
+async def load(ctx, extension):
+    try:
+        if str(extension) == "all" or str(extension) == "All":
+            for filename in os.listdir('./cogs'):
+                if filename.endswith('.py'):
+                    client.load_extension(f'cogs.{filename[:-3]}')
+        else:
+            client.load_extension(f'cogs.{extension}')
+            await ctx.send(f'{extension} was loaded successfully!')
+    except:
+        await ctx.send(f"{extension} has already been loaded or doesn't exsit")
+
+@client.command(aliases = ["unld"], help = "<ADMIN only command> Unload extension")
+@commands.has_permissions(administrator = True)
+async def unload(ctx, extension):
+    try:
+        if str(extension) == "all" or str(extension) == "All":
+            for filename in os.listdir('./cogs'):
+                if filename.endswith('.py'):
+                    client.unload_extension(f'cogs.{filename[:-3]}')
+        else:
+            client.unload_extension(f'cogs.{extension}')
+            await ctx.send(f'{extension} was unloaded successfully!')
+    except:
+        await ctx.send(f"{extension} has already been unloaded or doesn't exsit")
+
+@client.command(aliases = ["reld"], help = "<ADMIN only command> Reload extension")
+@commands.has_permissions(administrator = True)
+async def reload(ctx, extension):
+    try:
+        client.unload_extension(f'cogs.{extension}')
+        client.load_extension(f'cogs.{extension}')
+        await ctx.send(f'{extension} was reloaded successfully!')
+    except:
+        await ctx.send(f"{extension} doesn't exsit")
+
+
 
 
 '''
@@ -424,10 +552,34 @@ I would be very much appreciated. He will start giving you support. Thank you to
 
 
 
+
+
 @dice.error
 async def dice_error(ctx, error):
+    global default_footer
+    global default_footer_icon
     if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send(f'Appropriate usage: \n {pre}dice <value 1> <value 2>')
+        embed = discord.Embed(
+            title = "ERROR!",description = "This is an error message"
+        )
+        embed.add_field(name = "Appropraite usage:" , value = f"{pre}dice <value 1> <value 2>")
+        embed.set_footer(text = default_footer, icon_url = default_footer_icon)
+        await ctx.send(embed = embed)
+
+@load.error
+async def load_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send(f'{ctx.author} has no access to this command')
+
+@unload.error
+async def unload_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send(f'{ctx.author} has no access to this command')
+
+@reload.error
+async def reload_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send(f'{ctx.author} has no access to this command')
 
 
 for filename in os.listdir('./cogs'):
@@ -436,4 +588,4 @@ for filename in os.listdir('./cogs'):
 
 
 
-client.run('ODY1NDY1MDIyMjY0Mzc3Mzc2.YPEZHA.e-KjPU5UZtukWhrJQmfqzXmOXOo')
+client.run('')
